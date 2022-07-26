8ed80482372a669cb649c57e6a0a0925e9b7e560
hkwook <hkw1227@naver.com>
2022-07-26 12:33:57 +0900

    4 commit

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 # main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-2"
 }
